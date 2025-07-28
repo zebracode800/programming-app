62ace39d614c640eb7d9ce5a86c897dffa6ba6ec
Juu <ooucll0@gmail.com>
2025-07-28 09:47:28 -0300

    primeira aula

--- nome.js
+++ nome.js
@@ -3,5 +3,5 @@ const prompt = require("prompt-sync")();
 let nome = prompt("Digite o seu nome: ");
 let idade = prompt("Digite sua idade: ");
 
-console.log("\n\nSeu nome é", nome);
-console.log("Você tem", idade,"anos");
+console.log(`\n\nSeu nome é ${nome}`);
+console.log(`Você tem  idade ${anos}`);
