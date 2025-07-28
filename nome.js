6f9ca5a81c00fc2a174fae3f894d5351be705559
Juu <ooucll0@gmail.com>
2025-07-28 09:45:49 -0300

    primeira aula

--- nome.js
+++ nome.js
@@ -1,2 +1,7 @@
-console.log('oi world!');
-..
+const prompt = require("prompt-sync")();
+
+let nome = prompt("Digite o seu nome: ");
+let idade = prompt("Digite sua idade: ");
+
+console.log("\n\nSeu nome é", nome);
+console.log("Você tem", idade,"anos");
