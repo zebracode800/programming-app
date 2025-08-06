b133d338e97f17a7a2e4355dcdc95bf292df4fb1
Juu <ooucll0@gmail.com>
2025-08-06 11:26:10 -0300

    a

--- matriz.js
+++ matriz.js
@@ -2,33 +2,20 @@ const prompt = require("prompt-sync")();
 
 let times = ['Coritiba', 'Athletico', 'Maringa', 'Operario', 'Londrina'];
 
-times.push("Parana");
-times[1] = "Rio Branco";
+times.push(`Parana`);
+times[1] = `Rio Branco`;
 times.pop();
-times.unshift("Cascavel");
+times.unshift(`Cascavel`);
 times.shift();
 console.log(times.length);
 
 console.log(times);
 
+//arraw
 
-let quantidadeSalarios = prompt("Quantos salários você deseja calcular a média?");
-quantidadeSalarios = parseInt(quantidadeSalarios); 
-
-let somaSalarios = 0;
-let salarios = [];
-
-for (let i = 0; i < quantidadeSalarios; i++) {
-    let salarioAtual = prompt(`Digite o valor do salário °${i + 1}:`);
-    salarioAtual = parseFloat(salarioAtual);
-
-    somaSalarios += salarioAtual;
-
-    salarios.push(salarioAtual);
-}
-
-let mediaSalarios = somaSalarios / quantidadeSalarios;
-
-console.log("Salários digitados: ", salarios);
-console.log("-----------------------");
-console.log(`A média dos salários é: R$ ${mediaSalarios.toFixed(2)}`);
+let matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+console.log(`Valor da linha 1 coluna 2 é: ${matriz[0][1]}`);
--- nome.js
+++ nome.js
@@ -5,3 +5,10 @@ let idade = prompt("Digite sua idade: ");
 
 console.log(`\n\nSeu nome é ${nome}`);
 console.log(`Você tem  idade ${idade}`);
+
+let matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+console.log(`Valor da linha 1 coluna 2 é: ${matriz[0][1]}`);
